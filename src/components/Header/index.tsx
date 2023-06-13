867b07fb58e914434bc3b2ddef5e5934cff44885
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-06-13 11:48:17 -0300

    style: Visual structure - New transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { HeaderContainer, HeaderContent, NewTransactionsButton } from "./styles"
 import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -9,21 +10,13 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-    <Dialog.Root>
-      <Dialog.Trigger asChild>
-    <NewTransactionsButton>Nova transação</NewTransactionsButton>
-      </Dialog.Trigger>
+        <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionsButton>Nova transação</NewTransactionsButton>
+        </Dialog.Trigger>
 
-      <Dialog.Portal>
-        <Dialog.Overlay />
-        <Dialog.Title>Nova transação</Dialog.Title>
-
-        <Dialog.Close />
-        <Dialog.Content>
-
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+        <NewTransactionModal />
+      </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
